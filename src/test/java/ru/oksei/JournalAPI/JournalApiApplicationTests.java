7b2e7d45776153185c7a164e0a3a1e2fbe30ef7c
vitaly06 <vitaly.sadikov1@yandex.ru>
2024-11-07 20:29:43 +0500

    create db and add tables

---
+++ src/test/java/ru/oksei/JournalAPI/JournalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.oksei.JournalAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
